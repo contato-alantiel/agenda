1e75bdf2345896c4488b8a3727073145dc55ee0e
Alantiel Freire Marins <alantiel@alantiel.com>
2017-04-25 19:46:00 -0300

    Andamento cadastro e agenda semanal

--- assets/js/customers.js
+++ assets/js/customers.js
@@ -1,141 +1,157 @@
 $( document ).ready(function() {
-
-	//var key = null;
-   // Simply open the database once so that it is created with the required tables
-   $.indexedDB("RodrigoFisio", {
-   	"schema": {
-   		"1": function(versionTransaction){
-   			var customer = versionTransaction.createObjectStore("customer", {
-					//"autoIncrement": false,
-            	"keyPath": "customerEmail"
-   			});
+	var key = null;
+	// Simply open the database once so that it is created with the required tables
+	$.indexedDB("RodrigoFisio", {
+		"schema": {
+			"1": function(versionTransaction){
+				var customer = versionTransaction.createObjectStore("customer", {
+					"keyPath": "customerId"
+				});
 				
-				customer.createIndex("customerName");
-   			//customer.createIndex("customerEmail");
-   		}
-   	}
-   }).done(function(){
-   	// Once the DB is opened with the object stores set up, show data from all tables
-   	window.setTimeout(function(){
-			//emptyDB("customer");
-   		loadFromDB("customer");
-
+				customer.createIndex("customerId");
+			}
+		}
+	}).done(function(){
+	// Once the DB is opened with the object stores set up, show data from all tables
+	window.setTimeout(function(){
+		//emptyDB("customer");
+		loadFromDB("customer");
    		//downloadCatalog();
    	}, 200);
    });
 
 	// Iterate over each record in a table and display it
    function loadFromDB(table){
-   	emptyTable(table);
-   	_($.indexedDB("RodrigoFisio").objectStore(table).each(function(elem){
-   		addRowInHTMLTable(table, elem.key, elem.value);
-   	}));
+		emptyTable(table);
+		_($.indexedDB("RodrigoFisio").objectStore(table).each(function(elem){
+			addRowInHTMLTable(table, elem.key, elem.value);
+		}));
    }
 
 	function emptyDB(table){
-   	_($.indexedDB("RodrigoFisio").objectStore(table).clear());
-   }
+		_($.indexedDB("RodrigoFisio").objectStore(table).clear());
+	}
 
 	function emptyTable(tableName){
-   	var table = document.getElementById(tableName);
-   	var header = table.getElementsByTagName("tr")[0];
-   	table.innerHTML = "";
-   	header && table.appendChild(header);
-   }
+		var table = document.getElementById(tableName);
+		table.getElementsByTagName("tbody")[0].innerHTML = "";
+	}
 
-	function addRowInHTMLTable(tableName, key, value){
-   	var actions = {
-   		"customer": {
-   			"Add to cart": "editCustomer",
-   			"Add to wishlist": "removeFromCustomer"
-   		}
-   	}
-   	table = document.getElementById(tableName);
-   	var row = document.createElement("tr");
-   	var html = ["<tr>"];
-   	html = html.concat(["<td class = 'key'>", key, "</td>"]);
-   	html.push("<td class = 'action'>");
-   	for (var action in actions[tableName]) {
-   		html = html.concat("<a href = 'javascript:", actions[tableName][action], "(", key, ")'>", action, "</a>");
-   	}
-   	html.push("</td>");
-		html = html.concat(["<td class = 'value'>", renderJSON(value), "</td>"]);
-   	html.push("</tr>");
-   	row.innerHTML = html.join("");
-   	table.appendChild(row);
-   }
+	function addRowInHTMLTable(tableName, key, values){
+		var actions = {
+			"customer": {
+   			"Editar": "editCustomer",
+   			"Ver agenda": "seeCustomerSchedule"
+		}
+		}
+   		table = document.getElementById(tableName).getElementsByTagName("tbody")[0];
+	   	var row = document.createElement("tr");
+   		var html = ["<tr>"];
 
-	function renderJSON(val){
-   	var result = [];
-   	for (var key in val) {
-   		result.push("<div class = 'keyval'>");
-   		result.push("<span class = 'key'>", key, "</span>");
-   		result.push("<span class = 'value'>", JSON.stringify(val[key]), "</span>");
-   		result.push("</div>")
-   	}
-   	return result.join("");
-   }
+		html = html.concat([renderTD(values, 'customerId')]);
+		html = html.concat([renderTD(values, 'customerName')]);
+		html = html.concat([renderTD(values, 'customerEmail')]);
 
-	
+   		html.push("<td class = 'action'>");
+	   	for (var action in actions[tableName]) {
+   			html = html.concat("<a href = 'javascript:", actions[tableName][action], "(", key, ")'>", action, "</a> ");
+	   	}
+   		html.push("</td>");
+	   	html.push("</tr>");
+   		row.innerHTML = html.join("");
+   		table.appendChild(row);
+	}
 
-   function addToCustomer(customerOBJ){
+	function renderTD(obj, key){
+		var result = [];
+		result.push("<td class='", key, "'>");
+		result.push(obj[key]);
+		result.push("</td>")
+
+		return result.join("");
+	}
+
+	function addToCustomer(customerOBJ){
 		console.log(customerOBJ);
 		
 		var transaction = $.indexedDB("RodrigoFisio").transaction(['customer'], 'readwrite');
 
-   	transaction.done(function(){
-			console.log("yea");
-   		loadFromDB("customer");
-   	});
+		transaction.done(function(){
+			loadFromDB("customer");
+		});
 
-   	transaction.progress(function(transaction){
+		transaction.progress(function(transaction){
 			transaction.objectStore("customer").count().then(function(result) {
 				console.log("Transaction OK here");
 				customerOBJ['customerId'] = result+1;
 
 				transaction.objectStore("customer").add(customerOBJ).fail(function(e){
-					alert("putz");
-					console.log(e);
+					alert('Ocorreu algum erro, por favor verifique os campos e tente novamente...');
 				}).done(function(){
-					//_(customer.add(customerOBJ));
+					alert('Cliente salvo com sucesso.');
+					$("form:visible")[0].reset()
 					console.log("finishing");
 				});
 			});
    			
-   	});
-   }
+		});
+	}
    
-   function removeFromCustomer(itemId){
-   	$.indexedDB("RodrigoFisio").objectStore("customer")["delete"](itemId).done(function(){
-   		loadFromDB("customer");
-   	});
-   }
+	function removeFromCustomer(itemId){
+		$.indexedDB("RodrigoFisio").objectStore("customer")["delete"](itemId).done(function(){
+			loadFromDB("customer");
+		});
+	}
 
 	function _(promise){
-   	promise.then(function(a, e){
-   		console.log("Action completed", e.type, a, e);
-   	}, function(a, e){
-   		console.log("Action completed", a, e);
-   	}, function(a, e){
-   		console.log("Action completed", a, e);
-   	})
-   }
+			promise.then(function(a, e){
+   			console.log("Action completed", e.type, a, e);
+		}, function(a, e){
+   			console.log("Action completed", a, e);
+		}, function(a, e){
+			console.log("Action completed", a, e);
+		})
+	}
 
 	/*Add Customer*/
 	$('#add-customer').click(function (e) {
+		e.stopPropagation();
+		e.preventDefault();
+
+		var isValid = true; //TODO validate
+
+		if (isValid) {
+			var customer = {};
+		     
+			customer.customerName = $("#customer-name").val();
+			customer.customerEmail = $("#customer-email").val();
+			addToCustomer(customer);
+		}
+	});
+
+	/*See customer list/form*/
+	$('#see-customer-list').click(function (e) {
+		  e.stopPropagation();
+		  e.preventDefault();
+		
+		  var objThis = $(e.target);
+		  var objCustomerList = $("#customer-list");
+		  objCustomerList.parent().find("form").toggleClass("hide");
+		  objCustomerList.toggleClass("hide");
+		  objThis.parent().find("span").toggleClass("hide");
+	});
+
+	/*See schedule daily/weekly*/
+	$('.schedule-toggle-link').click(function (e) {
 		  e.stopPropagation();
 		  e.preventDefault();
 
-		  var bValid = true;
-
-		  if (bValid) {
-		     var customer = {};
-		     
-		     customer.customerName = $("#customer-name").val();
-		     customer.customerEmail = $("#customer-email").val();
-		     customer.customerDeleted = 0;
-		     addToCustomer(customer);
-		  }
+		  var objThis = $(e.target);
+		  if(objThis.is(".active")) return;
+		
+		  $(".schedule-toggle-link").toggleClass("active");
+		  $(".schedule-toggle").toggleClass("hide");
 	});
+
 
 });
